6c2f82afb998e9f9ccb49da077ca61f7c86a0af0
Jacek Kosinski <kashiash@gmail.com>
2022-11-29 22:22:15 +0100

    Initial Commit

---
+++ CurrencyRates/CurrencyRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyRatesApp.swift
+//  CurrencyRates
+//
+//  Created by Jacek Kosiński G on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
